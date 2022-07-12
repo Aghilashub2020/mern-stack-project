02dc88a6c91b0a10eb683b4fbb9f77f8af8b6eff
JShinault0620 <jshincool@gmail.com>
2022-07-12 18:50:27 -0400

    Added mongoose find to messages and rooms

--- backend/controllers/messages-controller.js
+++ backend/controllers/messages-controller.js
@@ -1,30 +1,36 @@
 // Requiring dependencies
 const express = require("express");
 const messagesController = express.Router();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
 
 // Requiring model
-const Message = require("../models/Message")
+const Message = require("../models/Message");
 
 // Routes
-messagesController.get("/", (req, res) => {
-    res.send("Welcome to the messagesController endpoint.")
+
+// Index
+messagesController.get("/", async (req, res) => {
+    const foundMessages = await Message.find();
+    res.send(foundMessages);
 });
 
 messagesController.post("/", jsonParser, (req, res) => {
-    console.log("Posted new message", req.body)
-    let newMessage = new Message(req.body)
+    console.log("Posted new message", req.body);
+    let newMessage = new Message(req.body);
     newMessage.save((err) => {
         if (err) {
-            console.log(err)
-        } else {
-
+            console.log(err);
         }
-    })
+    });
+    res.send(req.body);
+});
 
-    res.send(req.body)
+// Room parameter
+messagesController.get("/:room", jsonParser, async (req, res) => {
+    const foundMessages = await Message.find({"Room": "62cdf897d3a8b46206fb291b"})
+    res.send(foundMessages)
 })
 
 module.exports = messagesController;
